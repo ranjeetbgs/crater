a2a7217c7b29c15dd290f1554b498a2e9c21f402
Ranjeet Kumar <ranjitbeg2009@gmail.com>
2022-09-26 18:55:47 +0530

    fix bug

--- app/Space/helpers.php
+++ app/Space/helpers.php
@@ -325,13 +325,13 @@ function amountInWordsUSD($num){
             $rettxt .= @$ones[$i]; 
         }
         elseif($i < 100){ 
-            $rettxt .= $tens[substr($i,0,1)]; 
-            $rettxt .= " ".$ones[substr($i,1,1)]; 
+            $rettxt .= @$tens[substr($i,0,1)]; 
+            $rettxt .= " ".@$ones[substr($i,1,1)]; 
         }
         else{ 
-            $rettxt .= $ones[substr($i,0,1)]." ".$hundreds[0]; 
-            $rettxt .= " ".$tens[substr($i,1,1)]; 
-            $rettxt .= " ".$ones[substr($i,2,1)]; 
+            $rettxt .= @$ones[substr($i,0,1)]." ".$hundreds[0]; 
+            $rettxt .= " ".@$tens[substr($i,1,1)]; 
+            $rettxt .= " ".@$ones[substr($i,2,1)]; 
         } 
         if($key > 0){ 
             $rettxt .= " ".$hundreds[$key]." "; 
@@ -345,11 +345,11 @@ function amountInWordsUSD($num){
     if($decnum > 0){ 
         $rettxt .= " and "; 
         if($decnum < 20){ 
-            $rettxt .= $decones[$decnum]; 
+            $rettxt .= @$decones[$decnum]; 
         }
         elseif($decnum < 100){ 
-            $rettxt .= $tens[substr($decnum,0,1)]; 
-            $rettxt .= " ".$ones[substr($decnum,1,1)]; 
+            $rettxt .= @$tens[substr($decnum,0,1)]; 
+            $rettxt .= " ".@$ones[substr($decnum,1,1)]; 
         }
         $rettxt = $rettxt." Fils"; 
     } 
