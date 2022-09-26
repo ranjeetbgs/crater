1da29de4fccd23e8510a0b81c21c324db0437c12
Ranjeet Kumar <ranjitbeg2009@gmail.com>
2022-09-26 21:00:55 +0530

    fix bug

--- app/Space/helpers.php
+++ app/Space/helpers.php
@@ -248,110 +248,81 @@ function amountInWords(float $number)
     return ($rupees ? 'Rupees '.$rupees . ' Only' : ''); // . $paise;
 }
 
-function amountInWordsUSD($num){ 
-    $decones = array( 
-                '01' => "One", 
-                '02' => "Two", 
-                '03' => "Three", 
-                '04' => "Four", 
-                '05' => "Five", 
-                '06' => "Six", 
-                '07' => "Seven", 
-                '08' => "Eight", 
-                '09' => "Nine", 
-                10 => "Ten", 
-                11 => "Eleven", 
-                12 => "Twelve", 
-                13 => "Thirteen", 
-                14 => "Fourteen", 
-                15 => "Fifteen", 
-                16 => "Sixteen", 
-                17 => "Seventeen", 
-                18 => "Eighteen", 
-                19 => "Nineteen" 
-                );
-    $ones = array( 
-                0 => " ",
-                1 => "One",     
-                2 => "Two", 
-                3 => "Three", 
-                4 => "Four", 
-                5 => "Five", 
-                6 => "Six", 
-                7 => "Seven", 
-                8 => "Eight", 
-                9 => "Nine", 
-                10 => "Ten", 
-                11 => "Eleven", 
-                12 => "Twelve", 
-                13 => "Thirteen", 
-                14 => "Fourteen", 
-                15 => "Fifteen", 
-                16 => "Sixteen", 
-                17 => "Seventeen", 
-                18 => "Eighteen", 
-                19 => "Nineteen" 
-                ); 
-    $tens = array( 
-                0 => "",
-                2 => "Twenty", 
-                3 => "Thirty", 
-                4 => "Forty", 
-                5 => "Fifty", 
-                6 => "Sixty", 
-                7 => "Seventy", 
-                8 => "Eighty", 
-                9 => "Ninety" 
-                ); 
-    $hundreds = array( 
-                "Hundred", 
-                "Thousand", 
-                "Million", 
-                "Billion", 
-                "Trillion", 
-                "Quadrillion" 
-                ); //limit t quadrillion 
-    $num = number_format($num,2,".",","); 
-     //return $num;
-    $num_arr = explode(".",$num); 
-    // return $num_arr[0];
-    $wholenum = $num_arr[0]; 
-    $decnum = $num_arr[1]; 
-    $whole_arr = array_reverse(explode(",",$wholenum)); 
-    krsort($whole_arr); 
-    $rettxt = ""; 
-    foreach($whole_arr as $key => $i){ 
-        if($i < 20){ 
-            $rettxt .= @$ones[$i]; 
-        }
-        elseif($i < 100){ 
-            $rettxt .= @$tens[substr($i,0,1)]; 
-            $rettxt .= " ".@$ones[substr($i,1,1)]; 
-        }
-        else{ 
-            $rettxt .= @$ones[substr($i,0,1)]." ".$hundreds[0]; 
-            $rettxt .= " ".@$tens[substr($i,1,1)]; 
-            $rettxt .= " ".@$ones[substr($i,2,1)]; 
-        } 
-        if($key > 0){ 
-            $rettxt .= " ".$hundreds[$key]." "; 
-        } 
+function amountInWordsUSD($number){ 
+    // return $number;
+    $number = '2000.00';
+    $word = numberToWord($number) . '  Dirhams';
+    //echo $word;
     
-    } 
-    $rettxt = $rettxt." Dirhams";
-
-    // return $decnum;
+    $explode = explode('.', $number);   //separate the cents 
+    if((int)@$explode[1]) $word = $word . ' and ' . numberToWord($explode[1])." Fils";
+    return $word; 
+      
     
-    if($decnum > 0){ 
-        $rettxt .= " and "; 
-        if($decnum < 20){ 
-            $rettxt .= @$decones[$decnum]; 
-        }
-        elseif($decnum < 100){ 
-            $rettxt .= @$tens[substr($decnum,0,1)]; 
-            $rettxt .= " ".@$ones[substr($decnum,1,1)]; 
-        }
-        $rettxt = $rettxt." Fils"; 
-    } 
-    return $rettxt;
 } 
+
+function numberToWord($number)
+    {
+        if (($number < 0) || ($number > 999999999)) {
+            return "$number";
+        }
+
+        $Gn = floor($number / 1000000);  /* Millions (giga) */
+        $number -= $Gn * 1000000;
+        $kn = floor($number / 1000);     /* Thousands (kilo) */
+        $number -= $kn * 1000;
+        $Hn = floor($number / 100);      /* Hundreds (hecto) */
+        $number -= $Hn * 100;
+        $Dn = floor($number / 10);       /* Tens (deca) */
+        $n = $number % 10;               /* Ones */
+
+        $res = "";
+
+        if ($Gn) {
+            $res .= numberToWord($Gn) . " Million";
+        }
+
+        if ($kn) {
+            $res .= (empty($res) ? "" : " ") .
+            numberToWord($kn) . " Thousand";
+        }
+
+        if ($Hn) {
+            $res .= (empty($res) ? "" : " ") .
+            numberToWord($Hn) . " Hundred";
+        }
+
+        $ones = array(
+            "", "One", "Two", "Three", "Four", "Five", "Six",
+            "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen",
+            "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eightteen",
+            "Nineteen"
+        );
+        $tens = array(
+            "", "", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty",
+            "Seventy", "Eigthy", "Ninety"
+        );
+
+        if ($Dn || $n) {
+            if (!empty($res)) {
+                //            $res .= " and "; 
+                $res .= " ";
+            }
+
+            if ($Dn < 2) {
+                $res .= $ones[$Dn * 10 + $n];
+            } else {
+                $res .= $tens[$Dn];
+
+                if ($n) {
+                    $res .= "-" . $ones[$n];
+                }
+            }
+        }
+
+        if (empty($res)) {
+            $res = "zero";
+        }
+
+        return $res;
+    }
